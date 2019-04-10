7dbe5526bbca50a5b251f0441e8feb1719522738
thtdiabetic <itzgeorget@gmail.com>
2019-04-10 17:04:58 +0100

    bootstrapping completed

--- appointmentSystem/grails-app/init/appointmentsystem/BootStrap.groovy
+++ appointmentSystem/grails-app/init/appointmentsystem/BootStrap.groovy
@@ -38,6 +38,98 @@ appDuration: '15',
 roomNumber: '5'
 ).save()
 
+def Doctor1 =new Doctor(
+	fullName: 'Mandeep Singh',
+	qualification: 'Medicine',
+	position: 'General Practitioner',
+	doctorEmail: 'mandeepsinging@mailhost.com',
+	password: 'letmein790',
+	doctorOffice: 'room 16',
+	doctorPhone: '07743449724',
+	bio: 'Lorem ipsum Im a good doctor ipsum lorem.'
+).save()
+
+def Doctor2 =new Doctor(
+	fullName: 'Norman Hart',
+	qualification: 'Medicine',
+	position: 'Heart Surgeon',
+	doctorEmail: 'normanlovesit@mailhost.com',
+	password: 'password12345',
+	doctorOffice: 'room 17',
+	doctorPhone: '07740654563',
+	bio: 'I operate on peoples hearts.'
+	).save()
+
+def Nurse1 =new Nurse(
+	nurseName: 'Catherine Buckshill',
+	qualifications: 'Nursing',
+	nurseEmail: 'cattyb123@mailhost.com',
+	nurseOffice: 'room 3',
+	nursePhone: '07234359543'
+	).save()
+
+def Nurse2 =new Nurse(
+	nurseName: 'William Hill',
+	qualifications: 'Nursing',
+	nurseEmail: 'willyhilly@mailhost.com',
+	nurseOffice: 'room 3',
+	nursePhone: '07742359344'
+	).save()
+
+def Prescrip1 =new Prescription(
+pharmacyName: 'Chesterfield Road pharmacy',
+prescripNumber: 1,
+medicine: 'Percocet',
+totalCost: 8,
+dateIssued: new Date ('26/12/2018'),
+patientPaying: 'yes'
+).save()
+
+def Prescrip2 =new Prescription(
+pharmacyName: 'Baslow Road pharmacy',
+prescripNumber: 2,
+medicine: 'Prozac',
+totalCost: 8,
+dateIssued: new Date ('22/8/2018'),
+patientPaying: 'yes'
+).save()
+
+def Recep1 =new Receptionist(
+recepName: 'Johnny Doeson',
+recepEmail: 'jonndoesntdo@john.com',
+recepUsername: 'johnnyvegas',
+recepPassword: 'johnnygoddit',
+recepPhone: '077936548372'
+).save()
+
+def Recep2 =new Receptionist(
+recepName: 'Ariana Smith',
+recepEmail: 'coolgirltwirl13@gmail.com',
+recepUsername: 'cooldoctor9',
+recepPassword: 'ilovejohnny',
+recepPhone: '077865263642'
+).save()
+
+def Surgery1 =new Surgery(
+name: 'Woodseats Surgery',
+address: '11 Cobnar Crescent',
+postcode: 'S8 APX',
+telephone: '0114 2977 871',
+numberOfpatients: '60',
+description: 'Lorem ipsum',
+openingTime: '9:00'
+).save()
+
+def Surgery2 =new Surgery(
+name: 'Totley Surgery',
+address: '11 Baslow Crescent',
+postcode: 'S17 BAX',
+telephone: '0114 2987 814',
+numberOfpatients: '60',
+description: 'Lorem ipsum',
+openingTime: '9:00'
+).save()
+
 
 
 
